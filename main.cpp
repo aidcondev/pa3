099c641a69a2c19407db8ea2ff82a1a660bc62e5
Abhishek Kalokhe <abhisheknkalokhe@gmail.com>
2024-10-20 19:37:49 -0400

    update

--- main.cpp
+++ main.cpp
@@ -43,11 +43,22 @@ int main(int argc, char **argv)
     std::string flag = argv[1];
     std::string filename = argv[2];
 
+    int nodes, edges, source;
+    std::vector<std::pair<int, int>> edge_list;
+    std::vector<std::pair<std::pair<int, int>, int>> A;
+    std::vector<std::pair<std::pair<int, int>, int>> A_T;
+    int edges_result;
+    std::vector<std::pair<std::pair<int, int>, int>> spgemm_result;
+    std::vector<int> bfs_result;
+    int tc_result=0;
+
+    
+
     if (rank == 0)
     {
         if (argc < 3)
         {
-            std::cerr << "Usage: " << argv[0] << " <-flag> <file> <args>" << std::endl;
+            std::cerr << "Usage: " << argv[0] << " <-flag> <input_file>" << std::endl;
             return 1;
         }
 
@@ -56,71 +67,69 @@ int main(int argc, char **argv)
             std::cerr << "Invalid flag: " << flag << std::endl;
             return 1;
         }
+
+        std::ifstream file(filename);
+        if (!file.is_open())
+        {
+            std::cerr << "Could not open file: " << filename << std::endl;
+            return 1;
+        }
+
+        file >> nodes >> source >> edges;
+
+        for (int i = 0; i < edges; ++i)
+        {
+            file >> edge_list.emplace_back().first >> edge_list.back().second;
+        }
+
+        for (int i = 0; i < edge_list.size(); i++)
+        {
+            A.push_back(std::make_pair(std::make_pair(edge_list[i].first, edge_list[i].second), 1));
+            A_T.push_back(std::make_pair(std::make_pair(edge_list[i].second, edge_list[i].first), 1));
+        }
+
+        std::sort(A.begin(), A.end());
+        std::sort(A_T.begin(), A_T.end());
+
+        file >> edges_result;
+
+        for (int i = 0; i < edges_result; ++i)
+        {
+            file >> spgemm_result.emplace_back().first.first >> spgemm_result.back().first.second >> spgemm_result.back().second;
+        }
+
+
+        bfs_result.resize(nodes);
+        for (int i = 0; i < nodes; i++)
+        {
+            file >> bfs_result[i];
+        }
+
+        file >> tc_result;
+
+
+        file.close();   
     }
 
+    MPI_Bcast(&nodes, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&source, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&edges, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&edges_result, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
     if (flag == "-spgemm")
     {
 
-        int m, h, n;
+        int a_rows = edges / size;
+        int b_rows = edges / size;
+        int c_rows = edges / size;
 
-        int nnz_A;
-        int nnz_B;
-        int nnz_C;
-        std::vector<std::pair<std::pair<int, int>, int>> coo_A;
-        std::vector<std::pair<std::pair<int, int>, int>> coo_B;
-        std::vector<std::pair<std::pair<int, int>, int>> coo_C;
-
-        if (rank == 0)
-        {
-            std::ifstream file(filename);
-            if (!file.is_open())
-            {
-                std::cerr << "Could not open file: " << filename << std::endl;
-                return 1;
-            }
-
-            file >> m >> h >> n;
-
-            file >> nnz_A;
-
-            for (int i = 0; i < nnz_A; ++i)
-            {
-                file >> coo_A.emplace_back().first.first >> coo_A.back().first.second >> coo_A.back().second;
-            }
-
-            file >> nnz_B;
-
-            for (int i = 0; i < nnz_B; ++i)
-            {
-                file >> coo_B.emplace_back().first.first >> coo_B.back().first.second >> coo_B.back().second;
-            }
-
-            file >> nnz_C;
-
-            for (int i = 0; i < nnz_C; ++i)
-            {
-                file >> coo_C.emplace_back().first.first >> coo_C.back().first.second >> coo_C.back().second;
-            }
-
-            file.close();
-        }
-
-        MPI_Bcast(&m, 1, MPI_INT, 0, MPI_COMM_WORLD);
-        MPI_Bcast(&h, 1, MPI_INT, 0, MPI_COMM_WORLD);
-        MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
-        int a_rows = m / size;
-        int b_rows = h / size;
-        int c_rows = m / size;
-
-        if (m % size > rank)
+        if (edges % size > rank)
         {
             a_rows++;
             c_rows++;
         }
 
-        if (h % size > rank)
+        if (edges % size > rank)
         {
             b_rows++;
         }
@@ -129,11 +138,11 @@ int main(int argc, char **argv)
         int b_send_rows[size];
         int a_send_indices[size + 1];
         int b_send_indices[size + 1];
-        int a_rows_to_rank[m];
-        int b_rows_to_rank[h];
+        int a_rows_to_rank[edges];
+        int b_rows_to_rank[edges];
         int c_send_rows[size];
         int c_send_indices[size + 1];
-        int c_rows_to_rank[m];
+        int c_rows_to_rank[edges];
 
         std::vector<int> a_send_counts(size);
         std::vector<int> b_send_counts(size);
@@ -147,15 +156,15 @@ int main(int argc, char **argv)
 
         for (int i = 0; i < size; i++)
         {
-            a_send_rows[i] = m / size;
-            c_send_rows[i] = m / size;
-            b_send_rows[i] = h / size;
-            if (m % size > i)
+            a_send_rows[i] = edges / size;
+            c_send_rows[i] = edges / size;
+            b_send_rows[i] = edges / size;
+            if (edges % size > i)
             {
                 a_send_rows[i]++;
                 c_send_rows[i]++;
             }
-            if (h % size > i)
+            if (edges % size > i)
             {
                 b_send_rows[i]++;
             }
@@ -189,28 +198,28 @@ int main(int argc, char **argv)
                 }
             }
 
-            for (int i = 0; i < coo_A.size(); i++)
+            for (int i = 0; i < A.size(); i++)
             {
-                a_send_counts[a_rows_to_rank[coo_A[i].first.first]] += 3;
-                a_send_data[a_rows_to_rank[coo_A[i].first.first]].push_back(coo_A[i].first.first);
-                a_send_data[a_rows_to_rank[coo_A[i].first.first]].push_back(coo_A[i].first.second);
-                a_send_data[a_rows_to_rank[coo_A[i].first.first]].push_back(coo_A[i].second);
+                a_send_counts[a_rows_to_rank[A[i].first.first]] += 3;
+                a_send_data[a_rows_to_rank[A[i].first.first]].push_back(A[i].first.first);
+                a_send_data[a_rows_to_rank[A[i].first.first]].push_back(A[i].first.second);
+                a_send_data[a_rows_to_rank[A[i].first.first]].push_back(A[i].second);
             }
 
-            for (int i = 0; i < coo_B.size(); i++)
+            for (int i = 0; i < A_T.size(); i++)
             {
-                b_send_counts[b_rows_to_rank[coo_B[i].first.first]] += 3;
-                b_send_data[b_rows_to_rank[coo_B[i].first.first]].push_back(coo_B[i].first.first);
-                b_send_data[b_rows_to_rank[coo_B[i].first.first]].push_back(coo_B[i].first.second);
-                b_send_data[b_rows_to_rank[coo_B[i].first.first]].push_back(coo_B[i].second);
+                b_send_counts[b_rows_to_rank[A_T[i].first.first]] += 3;
+                b_send_data[b_rows_to_rank[A_T[i].first.first]].push_back(A_T[i].first.first);
+                b_send_data[b_rows_to_rank[A_T[i].first.first]].push_back(A_T[i].first.second);
+                b_send_data[b_rows_to_rank[A_T[i].first.first]].push_back(A_T[i].second);
             }
 
-            for (int i = 0; i < coo_C.size(); i++)
+            for (int i = 0; i < spgemm_result.size(); i++)
             {
-                c_send_counts[c_rows_to_rank[coo_C[i].first.first]] += 3;
-                c_send_data[c_rows_to_rank[coo_C[i].first.first]].push_back(coo_C[i].first.first);
-                c_send_data[c_rows_to_rank[coo_C[i].first.first]].push_back(coo_C[i].first.second);
-                c_send_data[c_rows_to_rank[coo_C[i].first.first]].push_back(coo_C[i].second);
+                c_send_counts[c_rows_to_rank[spgemm_result[i].first.first]] += 3;
+                c_send_data[c_rows_to_rank[spgemm_result[i].first.first]].push_back(spgemm_result[i].first.first);
+                c_send_data[c_rows_to_rank[spgemm_result[i].first.first]].push_back(spgemm_result[i].first.second);
+                c_send_data[c_rows_to_rank[spgemm_result[i].first.first]].push_back(spgemm_result[i].second);
             }
 
             a_send_indices[0] = 0;
@@ -237,6 +246,7 @@ int main(int argc, char **argv)
             {
                 flat_c_send_data.insert(flat_c_send_data.end(), vec.begin(), vec.end());
             }
+
         }
 
         int a_recv_count = 0;
@@ -275,7 +285,9 @@ int main(int argc, char **argv)
 
         std::vector<std::pair<std::pair<int, int>, int>> C_computed;
         double start = MPI_Wtime();
-        spgemm(m, h, n, a_recv_data_coo, b_recv_data_coo, C_computed);
+
+        spgemm(edges, edges, edges, a_recv_data_coo, b_recv_data_coo, C_computed);
+
         double end = MPI_Wtime();
 
         std::sort(c_recv_data_coo.begin(), c_recv_data_coo.end());
@@ -290,6 +302,11 @@ int main(int argc, char **argv)
         {
             flag = 1;
         }
+
+        int C_send_count = C_computed.size();
+        int total_C_computed_size = 0;
+
+        MPI_Reduce(&C_send_count, &total_C_computed_size, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
         MPI_Reduce(&flag, &total_flag, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
@@ -313,66 +330,6 @@ int main(int argc, char **argv)
 
     else if (flag == "-bfs")
     {
-
-        int source;
-
-        std::ifstream file1(filename);
-        if (!file1.is_open())
-        {
-            std::cerr << "Could not open file: " << filename << std::endl;
-            return 1;
-        }
-
-        int nodes, edges;
-        std::vector<std::pair<int, int>> edge_list;
-        std::vector<int> result;
-
-        if (rank == 0)
-        {
-            std::string line;
-            nodes = 0, edges = 0;
-
-            while (std::getline(file1, line))
-            {
-
-                if (!line.empty())
-                {
-                    continue;
-                }
-
-                std::istringstream iss(line);
-                if (!(iss >> nodes >> edges >> source))
-                {
-                    std::cerr << "Error reading nodes, edges and source." << std::endl;
-                    return 1;
-                }
-                break;
-            }
-
-            for (int i = 0; i < edges; ++i)
-            {
-                int u, v;
-                if (!(file1 >> u >> v))
-                {
-                    std::cerr << "Error reading edge data." << std::endl;
-                    return 1;
-                }
-
-                if (u != v)
-                    edge_list.emplace_back(u, v);
-            }
-
-            result.resize(nodes);
-            for (int i = 0; i < nodes; ++i)
-            {
-                file1 >> result[i];
-            }
-
-            file1.close();
-        }
-
-        MPI_Bcast(&nodes, 1, MPI_INT, 0, MPI_COMM_WORLD);
-        MPI_Bcast(&source, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
         std::vector<int> send_counts(size);
         int nodes_to_rank[nodes];
@@ -452,7 +409,9 @@ int main(int argc, char **argv)
 
         int c_send_count = n_rows;
 
+        
         MPI_Gather(&c_send_count, 1, MPI_INT, &C_sendcounts[0], 1, MPI_INT, 0, MPI_COMM_WORLD);
+
 
         if (rank == 0)
         {
@@ -463,16 +422,22 @@ int main(int argc, char **argv)
             }
         }
 
+
         double start = MPI_Wtime();
         bfs(nodes, source, recv_data_coo, node_depth);
         double end = MPI_Wtime();
 
+
+        result_depth.resize(nodes);
+
+
         MPI_Gatherv(&node_depth[0], c_send_count, MPI_INT, &result_depth[0], &C_sendcounts[0], &displsC[0], MPI_INT, 0, MPI_COMM_WORLD);
+
 
         // Compare C vector and result vector for verification
         if (rank == 0)
         {
-            if (std::equal(result_depth.begin(), result_depth.end(), result.begin()))
+            if (std::equal(result_depth.begin(), result_depth.end(), bfs_result.begin()))
             {
                 std::cout << "BFS is correct" << std::endl;
                 std::cout << "Time taken by BFS: " << end - start << std::endl;
@@ -480,6 +445,7 @@ int main(int argc, char **argv)
             else
             {
                 std::cout << "BFS is incorrect" << std::endl;
+                std::cout << "Time taken by BFS: " << end - start << std::endl;
             }
         }
 
@@ -488,53 +454,6 @@ int main(int argc, char **argv)
 
     else if (flag == "-tc")
     {
-
-        std::ifstream file1(filename);
-        if (!file1.is_open())
-        {
-            std::cerr << "Could not open file: " << filename << std::endl;
-            return 1;
-        }
-
-        int nodes, edges, result;
-        std::vector<std::pair<int, int>> edge_list;
-        int send_count[size];
-
-        if (rank == 0)
-        {
-            std::string line;
-            nodes = 0, edges = 0;
-
-            while (std::getline(file1, line))
-            {
-
-                std::istringstream iss(line);
-                if (!(iss >> nodes >> edges))
-                {
-                    std::cerr << "Error reading nodes and edges." << std::endl;
-                    return 1;
-                }
-                break;
-            }
-
-            for (int i = 0; i < edges; ++i)
-            {
-                int u, v;
-                if (!(file1 >> u >> v))
-                {
-                    std::cerr << "Error reading edge data." << std::endl;
-                    return 1;
-                }
-                if (u != v)
-                    edge_list.emplace_back(u, v);
-            }
-
-            file1 >> result;
-            file1.close();
-        }
-
-        MPI_Bcast(&nodes, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
         std::vector<int> send_counts(size);
         int nodes_to_rank[nodes];
         int send_indices[size + 1];
@@ -585,6 +504,7 @@ int main(int argc, char **argv)
             {
                 flat_send_data.insert(flat_send_data.end(), vec.begin(), vec.end());
             }
+
         }
 
         int recv_count = 0;
@@ -599,21 +519,23 @@ int main(int argc, char **argv)
             recv_data_coo.push_back(std::make_pair(std::make_pair(recv_data[i], recv_data[i + 1]), recv_data[i + 2]));
         }
 
+
         int triangle_count = 0;
         double start = MPI_Wtime();
         triangle_count = triangle_counts(nodes, recv_data_coo);
         double end = MPI_Wtime();
 
+
         if (rank == 0)
         {
             std::cout << "Triangle Count: " << triangle_count << std::endl;
-            if (triangle_count == result)
+            if (triangle_count == tc_result)
             {
                 std::cout << "Triangle Counting is correct" << std::endl;
             }
             else
             {
-                std::cout << "Triangle Counting is incorrect and not equal to " << result << std::endl;
+                std::cout << "Triangle Counting is incorrect and not equal to " << tc_result << std::endl;
             }
             std::cout << "Time taken by Triangle Counting: " << end - start << std::endl;
         }
